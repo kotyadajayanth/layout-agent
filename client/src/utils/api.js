6abbe3ef372eedd36d848624217fcd0074cdc392
kotyadajayanth <151765131+kotyadajayanth@users.noreply.github.com>
2026-05-17 09:16:28 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://layout-agent-2mz8.onrender.com',
   timeout: 60000, // LLM calls can be slow
 });
 
